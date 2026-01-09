0fb97f546f9db541a0aeecb11e94c3cc8742384a
Sevy Harris <sevy.harris@gmail.com>
2026-01-09 16:37:15 -0500

    add script to set up peuqse run for explorer

--- bpe/simulation/run_peuqse.py
+++ bpe/simulation/run_peuqse.py
@@ -5,46 +5,14 @@ import sim_wrapper
 import yaml
 import os
 
-working_dir = '/home/moon/uncertainty_estimator/bpe/simulation/'
-
-experimental_yaml_file = os.path.join(working_dir, 'experiment.yaml')
-prior_yaml_file = os.path.join(working_dir, 'prior.yaml')
-with open(experimental_yaml_file) as f:
-    experimental_data = yaml.safe_load(f)
-
-with open(prior_yaml_file) as f:
-    prior_data = yaml.safe_load(f)
-
-prior_labels = prior_data['species_names'] + prior_data['reaction_equations']
-
-
-# get global info about the indices for key species
-# TODO make sure this is only defined in one place
-# mech_yaml = '/home/moon/uncertainty_estimator/cpox_pt/cpox_pt_20251229/cantera/chem_annotated.yaml'
-mech_yaml = '/home/moon/uncertainty_estimator/cpox_pt/cpox_pt_20251229/cantera/chem_annotated_noCH4X.yaml'
-with open(mech_yaml) as f:
-    mech_data = yaml.safe_load(f)
-species_compositions = [x['composition'] for x in mech_data['species']]
-
-def get_i_thing(ref_composition, composition_list):
-    """Helper function for getting the index of a species in a Cantera phase given its composition"""
-    for i in range(len(composition_list)):
-        if composition_list[i] == ref_composition:
-            return i
-    assert False, f"Could not find species with composition {ref_composition} in mechanism {mech_yaml}"
-
-i_CH4 = get_i_thing({'C': 1.0, 'H': 4.0}, species_compositions)
-i_O2 = get_i_thing({'O': 2.0}, species_compositions)
-i_H2O = get_i_thing({'H': 2.0, 'O': 1.0}, species_compositions)
-i_H2 = get_i_thing({'H': 2.0}, species_compositions)
-i_CO = get_i_thing({'C': 1.0, 'O': 1.0}, species_compositions)
-i_CO2 = get_i_thing({'C': 1.0, 'O': 2.0}, species_compositions)
-
-gas_index_order = [i_CH4, i_O2, i_H2O, i_H2, i_CO, i_CO2]
-print("Gas index order:", gas_index_order)
-gas_names_order = ['CH4 (mol/min)', 'O2 (mol/min)', 'H2O (mol/min)', 'H2 (mol/min)', 'CO (mol/min)', 'CO2 (mol/min)']
+working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # convert experimental data to the format expected by PEUQSE
+experimental_yaml_file = os.path.join(working_dir, 'experiment.yaml')
+with open(experimental_yaml_file) as f:
+    experimental_data = yaml.safe_load(f)
+gas_index_order = experimental_data['out_gas_indices']
+gas_names_order = experimental_data['out_gas_names']
 experimental_y_values = []
 experimental_y_uncertainties = []
 for key in gas_names_order:
@@ -52,6 +20,12 @@ for key in gas_names_order:
     experimental_y_uncertainties.append(experimental_data['Uncertainty ' + key])
 experimental_y_values = np.vstack(experimental_y_values)
 experimental_y_uncertainties = np.vstack(experimental_y_uncertainties)
+
+prior_yaml_file = os.path.join(working_dir, 'prior.yaml')
+with open(prior_yaml_file) as f:
+    prior_data = yaml.safe_load(f)
+prior_labels = prior_data['species_names'] + prior_data['reaction_equations']
+
 
 # Set up PEUQSE basic info
 PEUQSE.UserInput.responses['responses_abscissa'] = experimental_data['Distance (m)']
--- bpe/simulation/sim_wrapper.py
+++ bpe/simulation/sim_wrapper.py
@@ -1,20 +1,21 @@
+import yaml
 import simulation
 import numpy as np
 
-# TODO move this hard coding to its proper place in the setup script
 
-# hard code the indices of the parameters? Don't want to be looking this up inside the simulation wrapper
-my_prior_species_indices = [5, 3, 1, 7, 9]  # shouldn't be hard coded here
-my_prior_reaction_indices = [0, 1, 3]
-my_output_gas_species_indices = [3, 4, 6, 7, 8, 5]
+with open('experiment.yaml') as f:
+    experimental_data = yaml.safe_load(f)
+with open('prior.yaml') as f:
+    prior_data = yaml.safe_load(f)
+
+my_prior_species_indices = prior_data['species_indices']
+my_prior_reaction_indices = prior_data['reaction_indices']
+output_gas_species_indices = experimental_data['out_gas_indices']
 
 # Make hardcoded list of distance indices to copy into your simulation_wrapper (maybe this can be a global later...)
-# interpolation_indices = []
-# for i in range(len(dist_array)):
-#     interpolation_indices.append(int(np.argmin(np.abs(simulation.dist_array - dist_array[i]))))
-# print(interpolation_indices)
-interpolation_indices = [211, 238, 266, 294, 321, 349, 377, 404, 432, 460, 488, 515, 543, 571, 598, 626, 654, 681, 709, 737]
-
+interpolation_indices = []
+for i in range(len(experimental_data['Distance (m)'])):
+    interpolation_indices.append(int(np.argmin(np.abs(simulation.dist_array - experimental_data['Distance (m)'][i]))))
 
 # wrapper for PEUQSE to call monolith simulation and return results in the format expected by PEUQSE
 def simulation_wrapper(params):
@@ -40,7 +41,7 @@ def simulation_wrapper(params):
 
     # extract the results for the species of interest and the distances
     results = []
-    for i in my_output_gas_species_indices:
+    for i in output_gas_species_indices:
         species_results = []
         for j in interpolation_indices:
             species_results.append(gas_out[j, i])
